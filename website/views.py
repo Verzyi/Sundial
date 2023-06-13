11572abb79c255dfd7009ad443a12405872d3595
chris.becker <christopher.becker.j@gmail.com>
2023-06-13 16:13:26 -0500

    big changes

--- website/views.py
+++ website/views.py
@@ -1,85 +1,76 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import PowderBlends
 from . import db
 from flask_login import login_user, login_required, current_user
-from sqlalchemy.orm import Query, sessionmaker
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import select
 
-engine = create_engine("sqlite:///:memory:", echo=True)
-Base = declarative_base()
-Base.metadata.create_all(engine)
+views = Blueprint('views', __name__)
 
-Session = sessionmaker(bind=engine)
-session = Session()
-
-views = Blueprint('views',__name__)
-
-
-
+numbers = []
+weights = []
 
 
 @views.route('/')
 @login_required
 def home():
-    return render_template("home.html", user = current_user)
+    blends = PowderBlends.query.all()
+    return render_template("home.html", user=current_user, blends=blends)
 
 
-@views.route('/blend', methods= ['GET','POST'])
+@views.route('/blend', methods=['GET', 'POST'])
 @login_required
 def blend():
-	if request.method == 'POST':
-		if request.form.get('BlendNum') is not None:
-			print('test')
-			flash("found blend number ", category='success')
+    search = None
+    numbers = []
+    weights = []
 
-			if  len(request.form.get('BlendNum')) != 0: 
-				blendNumber = request.form.get('BlendNum')
-				if int(request.form.get('BlendNum')) > 1:
+    if request.method == 'POST':
+        if request.form.get('BlendNum') is not None:
+            if len(request.form.get('BlendNum')) != 0:
+                blendNumber = request.form.get('BlendNum')
 
-					
+                if int(request.form.get('BlendNum')) > 1:
+                    search = PowderBlends.query.filter_by(PowderBlendID=blendNumber).all()
 
-					#7362 good one to test
-					search = PowderBlends.query.filter_by(PowderBlendID= blendNumber).all()
+                    if search:
+                        flash("Found blend number: " + str(blendNumber), category='success')
+                        return render_template("blend.html", user=current_user, blendNumber=search)
+                    else:
+                        flash("No blend found: " + str(blendNumber), category='error')
+                        return redirect(url_for('views.blend'))
+                else:
+                    flash("Blend number must be positive: " + str(blendNumber), category='error')
+                    return redirect(url_for('views.blend'))
 
+        elif request.form.get('addBlendBtn') is not None:
+            blendNumber = request.form.get('BlendNumber')
+            weight = request.form.get('weight')
 
-					if search:
-						flash("found blend number ", category='success')
-						
-					
-						weight= PowderBlends.query.filter_by(PowderBlendID= blendNumber).with_entities(PowderBlends.AddedWeight).all()
-						oldBlendNumber= PowderBlends.query.filter_by(PowderBlendID= blendNumber).with_entities(PowderBlends.OldPowderBlendID).all()
-						lastWeight= PowderBlends.query.filter_by(PowderBlendID= blendNumber).with_entities(PowderBlends.AddedWeight).all()
+            if len(blendNumber) != 0 and len(weight) != 0:
+                if int(weight) > 1:
+                    numbers.append(blendNumber)
+                    weights.append(weight)
+                    flash("Blend added: " + str(blendNumber), category='success')
 
-						return render_template("blend.html", user = current_user, blendNumber = search)
-						# return render_template("blend.html", user = current_user, blendNumber = search.PowderBlendID, weight = weight, oldBlendNumber = oldBlendNumber, lastWeight = lastWeight)
-
-					else:
-						flash("no blend found "+str(blendNumber), category='error')
-						
-				else:
-					flash("Blend number Must be postive "+str(blendNumber), category='error')
-					
-			else:
-				print("working on other")
-
-		elif request.form.get('BlendNumber') is not None and request.form.get('weight') is not None:
-			flash("found blend number", category='success')
-			if  len(request.form.get('BlendNumber')) != 0: 
-				blendNumber = request.form.get('BlendNumber')
-				if int(request.form.get('BlendNumber')) > 1:
-					search = PowderBlends.query.filter_by(PowderBlendID= blendNumber).all()
-
-					if search:
-						flash("found blend number", category='success')
+    return render_template("blend.html", user=current_user, blendNumber=search, numbers=numbers, weights=weights)
 
 
+@views.route('/create_blend', methods=['POST'])
+@login_required
+def create_blend():
+    blendNumber = request.form.get('BlendNumber')
+    weight = request.form.get('weight')
+
+    if len(blendNumber) != 0 and len(weight) != 0:
+        if int(weight) > 1:
+            numbers.append(blendNumber)
+            weights.append(weight)
+            flash("Blend added: " + str(blendNumber), category='success')
+
+    return render_template("blend.html", user=current_user, blendNumber=search, numbers=numbers, weights=weights)
 
 
-	return render_template("blend.html", user = current_user)
 
 @views.route('/builds')
 @login_required
 def builds():
-    return render_template("home.html", user = current_user)
+    return render_template("home.html", user=current_user)
