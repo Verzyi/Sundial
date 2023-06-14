053fcf893281af2b5eed75e7d1fa9750a81ecf46
chris.becker <christopher.becker.j@gmail.com>
2023-06-14 10:50:30 -0500

    split up blend into smaller html so that code work

--- website/views.py
+++ website/views.py
@@ -54,23 +54,62 @@ def blend():
     return render_template("blend.html", user=current_user, blendNumber=search, numbers=numbers, weights=weights)
 
 
-@views.route('/create_blend', methods=['POST'])
-@login_required
-def create_blend():
-    blendNumber = request.form.get('BlendNumber')
-    weight = request.form.get('weight')
-
-    if len(blendNumber) != 0 and len(weight) != 0:
-        if int(weight) > 1:
-            numbers.append(blendNumber)
-            weights.append(weight)
-            flash("Blend added: " + str(blendNumber), category='success')
-
-    return render_template("blend.html", user=current_user, blendNumber=search, numbers=numbers, weights=weights)
-
-
 
 @views.route('/builds')
 @login_required
 def builds():
     return render_template("home.html", user=current_user)
+
+
+
+
+@views.route('/searchBlends' , methods=['GET', 'POST'])
+@login_required
+def searchBlends():
+        
+    if request.method == 'POST':
+        if request.form.get('BlendNum') is not None:
+            if len(request.form.get('BlendNum')) != 0:
+                blendNumber = request.form.get('BlendNum')
+
+                if int(request.form.get('BlendNum')) > 1:
+                    search = PowderBlends.query.filter_by(PowderBlendID=blendNumber).all()
+
+                    if search:
+                        flash("Found blend number: " + str(blendNumber), category='success')
+                        return render_template("searchBlends.html", user=current_user, blendNumber=search)
+                    else:
+                        flash("No blend found: " + str(blendNumber), category='error')
+                        return redirect(url_for('views.searchBlends'))
+                else:
+                    flash("Blend number must be positive: " + str(blendNumber), category='error')
+                    return redirect(url_for('views.searchBlends'))
+                
+    return render_template("searchBlends.html", user=current_user)
+
+
+@views.route('/createBlend', methods=['GET', 'POST'])
+@login_required
+def create_blend():
+    if request.method == 'POST':
+        blendNumber = request.form.getlist('BlendNumber[]')
+        weight = request.form.getlist('weight[]')
+
+        for i in range(len(blendNumber)):
+            current_blend_number = blendNumber[i]
+            current_weight = weight[i]
+
+            if current_blend_number and current_weight:
+                if len(current_blend_number) != 0 and len(current_weight) != 0:
+                    if int(current_weight) > 1:
+                        search = PowderBlends.query.filter_by(PowderBlendID=current_blend_number).first()
+
+                        if search:
+                            flash("Found blend number: " + str(current_blend_number), category='success')
+                            numbers.append(current_blend_number)
+                            weights.append(current_weight)
+
+    return render_template("createBlend.html", user=current_user, numbers=numbers, weights=weights)
+
+
+
