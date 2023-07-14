80929c3912b54bf49390102192762cc6c5720ccb
chris.becker <cjbgamer555@gmail.com>
2023-07-14 15:05:20 -0500

    trying to fix the fade for flashs

--- website/static/js/script.js
+++ website/static/js/script.js
@@ -13,7 +13,26 @@
 
   function updateFormAction(event) {
     let form = document.getElementById('facilityForm');
-    let selectedOption = event.target.value;
-    form.action = '/builds/' + selectedOption;
+  let selectedOption = event.target.value;
+  form.action = '/builds/' + selectedOption;
+    
   }
+
+  // Function to automatically fade out flash messages after a few seconds
+  function fadeOutFlashMessages() {
+        const flashMessages = document.querySelectorAll('.alert-dismissible');
+
+        flashMessages.forEach((message) => {
+    setTimeout(() => {
+      message.classList.add('fade');
+      setTimeout(() => {
+        message.style.display = 'none';
+      }, 500); // Adjust the duration (in milliseconds) for how long the message stays visible before fading out
+    }, 3000); // Adjust the duration (in milliseconds) for how long the message stays visible before starting to fade out
+        });
+    }
+
+  // Call the function to fade out flash messages when the page finishes loading
+  window.addEventListener('load', fadeOutFlashMessages);
+
 </script>
