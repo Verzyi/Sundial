9fcdedae9df87c1fb26c99b5d7811a325279e1c1
joelsamiam <128829220+joelsamiam@users.noreply.github.com>
2023-09-29 15:21:57 -0500

    builds fixes, db update

--- website/auth.py
+++ website/auth.py
@@ -12,20 +12,18 @@ bcrypt = Bcrypt()
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        email = request.form.get('email')
+        email = request.form.get('email').lower()
         password = request.form.get('password')
         user = Users.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
-                flash('Logged in successfully', category='success')
+                flash('Logged in successfully.', category='success')
                 login_user(user, remember=True)
-                # current_user.set_ip = request.remote_addr
-                # db.session.commit()
                 return redirect(url_for('views.Home'))
             else:
-                flash('Password incorrect', category='error')
+                flash('Password incorrect!', category='error')
         else:
-            flash('Email does not exist', category='error')
+            flash('Email does not exist!', category='error')
 
     return render_template('login.html', user=current_user)
 
@@ -37,10 +35,10 @@ def logout():
     session['_remember'] = 'clear'
     return redirect(url_for('auth.login'))
 
-@auth.route('/sign-up', methods=['GET', 'POST'])
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        email = request.form.get('email')
+        email = request.form.get('email').lower()
         first_name = request.form.get('firstName')
         last_name = request.form.get('lastName')
         password1 = request.form.get('password1')
@@ -48,17 +46,17 @@ def signup():
 
         user = Users.query.filter_by(email=email).first()
         if user:
-            flash('Email already exists', category='error')
+            flash('Email already exists!', category='error')
         elif len(email) < 4:
-            flash('Email must be greater than 4 characters.', category='error')
+            flash('Email must be greater than 4 characters!', category='error')
         elif len(first_name) < 2:
-            flash('First name must be greater than 1 character.', category='error')
+            flash('First name must be greater than 1 character!', category='error')
         elif len(last_name) < 2:
-            flash('Last name must be greater than 1 character.', category='error')
+            flash('Last name must be greater than 1 character!', category='error')
         elif password1 != password2:
-            flash('Passwords do not match', category='error')
+            flash('Passwords do not match!', category='error')
         elif len(password1) < 6:
-            flash('Password must be greater than 6 characters.', category='error')
+            flash('Password must be greater than 6 characters!', category='error')
         else:
             hashed_password = bcrypt.generate_password_hash(password1).decode('utf-8')
             # Append Users
@@ -75,8 +73,8 @@ def signup():
             )
             db.session.add(new_user)
             db.session.commit()
-            flash('Account created', category='success')
+            flash('Account created.', category='success')
             login_user(new_user, remember=True)
             return redirect(url_for('views.Home'))
 
-    return render_template('sign-up.html', user=current_user)
+    return render_template('signup.html', user=current_user)
