ed4105830392c1ba4bb821c9f03b48b2e32a431e
chris.becker <cjbgamer555@gmail.com>
2023-10-16 12:20:08 -0500

    working on schduler

--- website/scheduler.py
+++ website/scheduler.py
@@ -18,9 +18,41 @@ scheduler = Blueprint('scheduler', __name__)
 @scheduler.route('/scheduler', methods=['GET', 'POST'])
 @login_required
 def index():
+    # Define your machines mapping
+    machines_mapping = {
+            "M1": "1849",
+            "M2": "1848",
+            "M3": "1476",
+            "M4": "1991",
+            "M5": "2001",
+            "M6": "2006",
+            "M7": "1989",
+            "M8": "1160",
+            "M9": "1852",
+            "M10": "1810",
+            "M11": "1853",
+            "M12": "1351",
+            "M13": "2643",
+            "M14": "2642",
+            "M15": "1882",
+            "M16": "3607",
+            "M17": "2813"
+        }
     
+    
+    builds = BuildsTable.query
+    AllBuilds = builds.filter(BuildsTable.MachineID.isnot(None))
+    builds = builds.filter(BuildsTable.MachineID.isnot(None))
+    builds = builds.filter(BuildsTable.BuildFinishTime==None)
+    builds = builds.order_by(desc(BuildsTable.BuildID)).all()
+    # Fetch all unique machine names and material names
+    machines = [build.MachineID for build in AllBuilds]
+    materials = [build.AlloyName for build in AllBuilds]
+    machines = list(set(machines))
+    materials = list(set(materials))
 
-    return render_template('scheduler/scheduler.html',user=current_user)
+
+    return render_template('scheduler/scheduler.html',user=current_user, machine_return =machines_mapping, builds= builds, machines = sorted(machines), materials= sorted(materials))
 
 
 
@@ -30,18 +62,13 @@ def unscheduled():
     
     builds = BuildsTable.query
     AllBuilds = builds.filter(BuildsTable.MachineID.isnot(None))
-    builds = builds.filter(or_(BuildsTable.MachineID==None, BuildsTable.MachineID=="None" ))
+    builds = builds.filter(or_(BuildsTable.MachineID==None, BuildsTable.MachineID=="None" , BuildsTable.MachineID=="" ))
     builds = builds.order_by(desc(BuildsTable.BuildID)).all()
     # Fetch all unique machine names and material names
     machines = [build.MachineID for build in AllBuilds]
     materials = [build.AlloyName for build in AllBuilds]
     machines = list(set(machines))
     materials = list(set(materials))
-    # Fetch the build information from the database based on the selected build ID (use the selectedBuildID variable)
-    selectedBuildID = request.form.get('BuildIDInput')
-    selectedBuild = BuildsTable.query.filter_by(BuildID=selectedBuildID).first()
-    selectedBuildCreatedOn = None
-    
 
     return render_template('scheduler/unscheduled-builds.html',user=current_user,builds= builds, machines = sorted(machines), materials= sorted(materials) )
 
@@ -57,6 +84,7 @@ def update_database():
     builds = builds.filter_by(BuildID=data["build_id"]).first()
     builds.MachineID = data["machine_id"]
     builds.AlloyName = data["material_name"]
+    builds.BuildTime = data['build_time']
     
     # Commit changes to the database
     db.session.commit()
@@ -64,3 +92,27 @@ def update_database():
     # Return a response indicating success or failure
     response_data = {'status': 'success', 'message': 'Database updated successfully'}
     return jsonify(response_data)
+
+
+
+
+
+
+@scheduler.route('/tasks', methods=['GET'])
+def get_tasks():
+    builds = BuildsTable.query.filter_by(BuildFinishTime=None).all()  # Fetch tasks where BuildFinishTime is NULL
+    # print(builds)
+    
+    # Convert the list of builds to a list of dictionaries
+    tasks = []
+    for build in builds:
+        tasks.append({
+            'BuildID': build.BuildID,
+            'AlloyName': build.AlloyName,
+            'MachineID': build.MachineID,
+            'BuildStart': build.BuildStartTime,
+            'BuildTime': build.BuildTime
+        })
+
+    # Return the tasks as JSON
+    return jsonify(tasks)
--- website/static/js/scheduler.js
+++ website/static/js/scheduler.js
@@ -1,14 +1,121 @@
+// Function to fetch tasks from the Flask API
+async function fetchTasks() {
+    try {
+        const response = await fetch('/tasks'); // Assuming your Flask API is hosted on the same domain
+        if (response.ok) {
+            const tasks = await response.json();
+            return tasks; // Return the fetched tasks data
+        } else {
+            console.error('Failed to fetch tasks:', response.status);
+            return []; // Return an empty array in case of error
+        }
+    } catch (error) {
+        console.error('Error fetching tasks:', error);
+        return []; // Return an empty array in case of error
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', async function() {
+    const allTasks = await fetchTasks(); // Wait for tasks to be fetched before proceeding
+
+    console.log('Fetched tasks:', allTasks); // Log the fetched tasks to the console
+
+    if (!Array.isArray(allTasks)) {
+        console.error('Invalid tasks data:', allTasks);
+        return;
+    }
+    
+
+    class Task {
+        constructor(buildID, alloyName, machineID, buildStart, buildTime) {
+            this.buildID = buildID;
+            this.alloyName = alloyName;
+            this.machineID = machineID;
+            this.buildStart = buildStart;
+            this.buildTime = buildTime;
+        }
+
+        static sortTasks(tasks) {
+            // Sort tasks by MachineID, BuildStart, BuildTime, and BuildID
+            tasks.sort((a, b) => {
+                if (a.machineID !== b.machineID) {
+                    return a.machineID - b.machineID;
+                }
+                if (a.buildStart !== b.buildStart) {
+                    return a.buildStart - b.buildStart;
+                }
+                if (a.buildTime !== b.buildTime) {
+                    return a.buildTime - b.buildTime;
+                }
+                return a.buildID - b.buildID;
+            });
+            return tasks;
+        }
+    }
+
+    // Create instances of the Task class
+    const tasks = allTasks.map(task =>
+        new Task(task.BuildID, task.AlloyName, task.MachineID, task.BuildStart, task.BuildTime)
+    );
+
+    // Sort tasks
+    const sortedTasks = Task.sortTasks(tasks);
+
+    // Group tasks by machine ID
+    const tasksByMachine = {};
+    sortedTasks.forEach(task => {
+        if (!(task.machineID in tasksByMachine)) {
+            tasksByMachine[task.machineID] = [];
+        }
+        tasksByMachine[task.machineID].push(task);
+    });
+
+    
+
+  // Print tasks separated by machines to console.log
+  console.log(tasksByMachine);
+
+  const container = document.getElementById('tasks-container');
+
+    for (const machineID in tasksByMachine) {
+        const machineTasks = tasksByMachine[machineID];
+
+        const machineDiv = document.createElement('div');
+        machineDiv.classList.add('machine-row');
+
+        // Display machine number
+        const machineNumber = document.createElement('div');
+        machineNumber.classList.add('machine-number');
+        machineNumber.textContent = `${machineID}`;
+        machineDiv.appendChild(machineNumber);
+
+        // Display tasks for this machine
+        machineTasks.forEach(task => {
+            const taskDiv = document.createElement('div');
+            taskDiv.classList.add('task');
+            taskDiv.textContent = `Task: ${task.buildID}, Time: ${task.buildTime || 'null'}`;
+            machineDiv.appendChild(taskDiv);
+        });
+
+        container.appendChild(machineDiv);
+    }
+});
+
+
 function updateDatabase(selectedValue, inputName) {
     // Get the build ID from your HTML, you can modify this as per your HTML structure
     let buildId = document.getElementById('buildId').textContent;
     let machine = document.getElementById('machineIdInput').value;
     let material = document.getElementById('AlloyNameInput').value;
+    let buildTime = document.getElementById('buildTime').value;
 
     // Prepare data to send in the AJAX request
     let data = {
         build_id: buildId,
         machine_id: machine,
-        material_name: material
+        material_name: material,
+        build_time: buildTime
     };
     console.log(data);
     // Make an AJAX request to the Flask route
@@ -34,3 +141,6 @@ function updateDatabase(selectedValue, inputName) {
         // Handle network or other errors here
     });
 }
+
+
+
