735ef976d0013936babea0683b0de49c3248e072
Chris Becker <cjbgamer555@gmail.com>
2024-01-08 17:34:13 -0600

    fixing aws

--- website/powder.py
+++ website/powder.py
@@ -18,7 +18,7 @@ try:
 except Exception as e:
     print(e)
     # Set the Lambda Layer path for wkhtmltopdf executable
-    WKHTMLTOPDF_PATH = '/opt/bin/wkhtmltopdf'  # Update the path accordingly
+    wkhtml_path = '/opt/bin/wkhtmltopdf'  # Update the path accordingly
 
 powder = Blueprint('powder', __name__)
 
